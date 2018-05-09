38dd7cb63ffaae520485cb970fdea2261aa3f7cf
Miguel Pignatelli <emepyc@gmail.com>
2018-05-09 22:58:47 +0200

    working simple example

--- examples/simple/example.js
+++ examples/simple/example.js
@@ -1,30 +1,19 @@
 (
   function (div) {
-    var newick = axios.get('/examples/speciestree.nwk')
-      .then(tree => {
-        return tree.data;
+    var theme = iHam()
+      .on('node_selected', function (node) {
+        d3.select('#current-node')
+          .text(node.node_name());
       })
-    var orthoxml = axios.get('/examples/hbb.orthoxml')
-      .then(orthoxml => {
-        return orthoxml.data;
-      });
+      .query_gene({
+        id: 12
+      })
+      .tree_width(500)
+      .board_width(800)
+      .orthoxml(data.orthoxml)
+      .newick(data.tree)
+      .fam_data(data.fam_data);
 
-    axios.all([orthoxml, newick])
-      .then(resps => {
-        var theme = iHam()
-          .on('node_selected', function (node) {
-            d3.select('#current-node')
-              .text(node.node_name());
-          })
-          .query_gene({
-            id: 12
-          })
-          .tree_width(500)
-          .board_width(800);
-        // .orthoxml(resps[0])
-        // .newick(resps[1]);
-
-        theme(div);
-      });
+    theme(div);
   }
 )(document.getElementById('iham'));
